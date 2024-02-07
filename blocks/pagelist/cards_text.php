87911ef8edeca895bd86ae05bd6ec9699623ced8
Christian Heinisch <chris@christian-heinisch.de>
2024-02-07 14:52:58 +0100

    Modify am-block class
    Edit CSS for +Main Block

--- blocks/pagelist/cards_text.php
+++ blocks/pagelist/cards_text.php
@@ -3,7 +3,7 @@
 		<@ set { :paddingTop: @{ cardImageRelativeHeight | def (75) } } @>
 		<@ foreach in pagelist @>
         <div class="col-lg-12 pb-3">
-            <h2><a href="@{ url }">@{ title }</a></h2>
+            <h2><a href="@{ url }" class="link-body-emphasis card-headline">@{ title }</a></h2>
             <div class="small text-muted">@{ date | dateFormat (@{ formatDate | def ('l, F jS Y')}, @{ locale }) } // Author's Name</div>
             <p>@{ +main | findFirstParagraph | 450 }</p>
         </div>
--- post.php
+++ post.php
@@ -3,28 +3,15 @@ This template uses includes for elements that might be used also in other templa
 such as the header, navbar and footer markup. 
 #>
 <@ elements/header.php @>
-	<@ elements/navbar.php @>
-	<section class="section">
-		<div class="container">
-			<div class="columns is-8 is-variable">
-				<main class="column is-three-quarters content">
-					<# 
-					Note that the <h1> has added the 'am-block' class.
-					This is essential to make the headline aligned with all other blocks
-					in the +main variable.
-					#>
-					<h1 class="am-block title">
-						@{ title }
-					</h1>
+<div class="container-md">
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    <div class="small text-muted pt-2 pb-1">January 1, 2023 // Author's Name</div>
 					@{ +main }
-				</main>
-				<nav class="column is-one-quater">
-					<# 
-					Take a look at the menu snippet file to actually see what's happening.
-					#>
-					<@ elements/menu.php @>
-				</nav>
-			</div>
-		</div>
-	</section>
+                    <div class="col-lg-12">Category: <a href="#">Test</a></div>
+                    <div class="col-lg-12">Tag: <a href="#">#Test</a></div>
+                </div>
+            </div>
+        </div>
+        <!-- Content Ende -->
 <@ elements/footer.php @>
