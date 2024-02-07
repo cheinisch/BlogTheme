d2cd7ca28404bc4912f9679ccd9095c50d314794
Christian Heinisch <chris@christian-heinisch.de>
2024-02-07 13:05:53 +0100

    make Post linkable
    Add Link to Blicklist Card Item

--- blocks/pagelist/cards_img.php
+++ blocks/pagelist/cards_img.php
@@ -6,9 +6,9 @@
             <div class="card-img-top">
                 <img class="card-img-top" src="https://bootstrapious.com/i/snippets/sn-static-header/background.jpg" />
             </div>
-            <h2>@{ title }</h2>
-            <div class="small text-muted">January 1, 2023 // Author's Name</div>
-            <p>@{ +main | findFirstParagraph | 150 }</p>
+            <h2><a href="@{ url }">@{ title }</a></h2>
+            <div class="small text-muted">@{ date | dateFormat (@{ formatDate | def ('l, F jS Y')}, @{ locale }) } // Author's Name</div>
+            <p>@{ +main | findFirstParagraph | 450 }</p>
         </div>
         <@ end @>
 	</div>
