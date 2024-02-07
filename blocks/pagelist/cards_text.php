3c1e7285622361666e0f49b58d10834fd7a35c9e
Christian Heinisch <chris@christian-heinisch.de>
2024-02-07 12:18:49 +0100

    Update cards_text.php
    Change LAyout

--- blocks/pagelist/cards_text.php
+++ blocks/pagelist/cards_text.php
@@ -4,7 +4,7 @@
 		<@ foreach in pagelist @>
         <div class="col-lg-12 pb-3">
             <h2>@{ title }</h2>
-            <div class="small text-muted">January 1, 2023 // Author's Name</div>
+            <div class="small text-muted">@{ date | dateFormat (@{ formatDate | def ('l, F jS Y')}, @{ locale }) } // Author's Name</div>
             <p>@{ +main | findFirstParagraph | 450 }</p>
         </div>
         <@ end @>
