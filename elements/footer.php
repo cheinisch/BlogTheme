95a03b9c825e0489675626db839b7f0b4f31117c
Christian Heinisch <chris@christian-heinisch.de>
2024-02-07 09:27:34 +0100

    Update footer.php
    Add Submenu

--- elements/footer.php
+++ elements/footer.php
@@ -13,11 +13,12 @@
             </ul>
 
             <ul class="nav col-md-4 justify-content-end">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+            <@ newPagelist { type: false, excludeHidden: false } @>
+			<@ foreach in pagelist @>
+				<@ if @{ checkboxShowInFooter } @>
+					<li><a href="@{ url }" class="nav-link px-2 text-body-secondary">@{ title }</a></li>
+				<@ end @>
+			<@ end @>	
             </ul>
         </footer>
     </div>
