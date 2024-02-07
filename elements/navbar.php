7893db119b2d64c8d56f1eccddb2f28de35818a8
Christian Heinisch <chris@christian-heinisch.de>
2024-02-07 10:00:27 +0100

    Update navbar.php
    Add Navmenu to Navbar

--- elements/navbar.php
+++ elements/navbar.php
@@ -7,21 +7,17 @@
                         </button>
                         <div class="collapse navbar-collapse" id="navbarCollapse">
                             <ul class="navbar-nav me-auto mb-2 mb-md-0 navbar-nav col-lg-12 justify-content-lg-end">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="index_mit_sidebar.html">List mit Sidebar</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="page.html">Page</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="article.html">Article</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                            </li>
+							<@ newPagelist { type: false, excludeHidden: false } @>
+								<@ foreach in pagelist @>
+									<@ if @{ checkboxShowInNavbar } @>
+										<@ if @{ :current } @>
+											<li class="nav-item active" aria-current="page"><a class="nav-link active" aria-current="page" href="@{ url }">@{ title }</a></li>
+										<@ else @>
+											<li class="nav-item"><a class="nav-link" href="@{ url }">@{ title }</a></li>
+										<@ end @>
+									<@ end @>
+								<@ end @>
+							<@ end @>
                             </ul>
                         </div>
                     </div>
