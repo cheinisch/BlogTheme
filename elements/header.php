73e44998ba035fea690625328a4c8a738e005502
Christian Heinisch <chris@christian-heinisch.de>
2024-03-13 15:24:42 +0100

    Add Dark Mode Auto Switch

--- elements/header.php
+++ elements/header.php
@@ -17,6 +17,19 @@
 	<link rel="stylesheet" href="/packages/@{ theme }/css/style.css" rel="stylesheet" />
 
 	<!-- Javascripte -->
+	<script>
+		(function () {
+			const htmlElement = document.querySelector("html")
+			if(htmlElement.getAttribute("data-bs-theme") === 'auto') {
+			function updateTheme() {
+				document.querySelector("html").setAttribute("data-bs-theme",
+				window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+			}
+				window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
+			updateTheme()
+			}
+		})();	
+	</script>		
 </head>
 <body>
 	<!-- Bootstrap Static Header -->
