10632bd30f2ce035754351b6ab4bf03376a066c6
Christian Heinisch <chris@christian-heinisch.de>
2024-03-20 08:54:21 +0100

    Update sidebar.php

--- elements/sidebar.php
+++ elements/sidebar.php
@@ -5,9 +5,6 @@
 
 <h3>Tags</h3>
 
-<@ foreach in tags 
-	@><a 
-	href="@{ urlTagLinkTarget | def (@{ :parent }) }?filter=@{ :tag }#filters"
-	class="@{ :color | def ('has-text-dark') }"
-	><@ if @{ :i } > 1 @>, <@ end @>@{ :tag }</a><@ 
-end @>
+<@ foreach in tags 	@>
+	<@ if @{ :i } > 1 @>, <@ end @><a href="@{ urlTagLinkTarget | def (@{ :parent }) }?filter=@{ :tag }#filters" class="@{ :color | def ('has-text-dark') }" >@{ :tag }</a>
+<@ end @>
